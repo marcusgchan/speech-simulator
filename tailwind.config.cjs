852a334cadeb88407b45a461ddaf34d34c502029
Kentaro Lim <kentarolim10@gmail.com>
2023-01-21 13:19:08 -0800

    add-color-styling-and-home-page-format

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'accent': '#10b981',
+      },
+    },
   },
   plugins: [],
 };
