195065a33f5fa68b1e03758f9ea1784efff26630
Danilo V <danilov2244@gmail.com>
2023-01-22 09:35:53 -0800

    Give white text to presentations back button

--- src/pages/presentations/index.tsx
+++ src/pages/presentations/index.tsx
@@ -38,7 +38,7 @@ export default function Index() {
           </h1>
           <button
             onClick={() => router.push("/")}
-            className="hover:bg-emeral-700 rounded-xl bg-accent py-2 px-4 hover:bg-emerald-700"
+            className="hover:bg-emeral-700 rounded-xl bg-accent py-2 px-4 text-white hover:bg-emerald-700"
           >
             Home Page
           </button>
