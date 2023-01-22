35f555b283f80773d88a8c54aea7a1800f390356
Kentaro Lim <kentarolim10@gmail.com>
2023-01-22 04:21:03 -0800

    Add vr data fetching

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -2,17 +2,18 @@ import { createTRPCRouter } from "./trpc";
 import { exampleRouter } from "./routers/example";
 import { presentationRouter } from "./routers/presentationRouter";
 import { attemptRouter } from "./routers/attemptsRouter";
+import { queueRouter } from "./routers/queueRouter";
 
 /**
  * This is the primary router for your server.
- *  
+ *
  * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
   presentation: presentationRouter,
-  
-  attempt: attemptRouter
+  queue: queueRouter,
+  attempt: attemptRouter,
 });
 
 // export type definition of API
