cd54052d67d9aff594e219c3cda84f3fd63a7054
Marcus Chan <marcusgchan35@gmail.com>
2023-01-22 07:57:06 -0800

    Fix typing

--- src/pages/presentations/index.tsx
+++ src/pages/presentations/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { RouterOutputs, api } from "../../utils/api";
+import { type RouterOutputs, api } from "../../utils/api";
 import { useRouter } from "next/router";
 
 export default function Index() {
@@ -18,38 +18,10 @@ export default function Index() {
 
   const isMobile = width <= 1000;
 
-  const router = useRouter();
-  const navigate = (path: string) => {
-    if (router.pathname !== path) {
-      router.push(path);
-    }
-  };
-
   const { data, isLoading, isError } = api.presentation.getAll.useQuery();
 
   if (!data || isLoading) return <div>loading</div>;
   if (isError) return <div>something died¡</div>;
-
-  const PresentationCard = ({
-    presentation,
-  }: {
-    presentation: NonNullable<RouterOutputs["presentation"]["getPresentation"]>;
-  }) => {
-    return (
-      <button
-        className="m-4 flex flex-col rounded-xl border-2 p-4 text-left"
-        onClick={() => navigate(`/presentations/${presentation.id}/`)}
-      >
-        <div className="h-4 w-full"></div>
-        <i>{presentation.createdAt.toDateString()}</i>
-        <h1 className="text-2xl font-extrabold">{presentation.title}</h1>
-        <div className="text-accent">
-          Goal time: {Math.round((presentation.idealTime / 60) * 100) / 100}{" "}
-          minutes
-        </div>
-      </button>
-    );
-  };
 
   return (
     <div>
@@ -73,3 +45,32 @@ export default function Index() {
     </div>
   );
 }
+const PresentationCard = ({
+  presentation,
+}: {
+  presentation: Omit<
+    NonNullable<RouterOutputs["presentation"]["getPresentation"]>,
+    "flashcards"
+  >;
+}) => {
+  const router = useRouter();
+  const navigate = (path: string) => {
+    if (router.pathname !== path) {
+      router.push(path);
+    }
+  };
+  return (
+    <button
+      className="m-4 flex flex-col rounded-xl border-2 p-4 text-left"
+      onClick={() => navigate(`/presentations/${presentation.id}/`)}
+    >
+      <div className="h-4 w-full"></div>
+      <i>{presentation.createdAt.toDateString()}</i>
+      <h1 className="text-2xl font-extrabold">{presentation.title}</h1>
+      <div className="text-accent">
+        Goal time: {Math.round((presentation.idealTime / 60) * 100) / 100}{" "}
+        minutes
+      </div>
+    </button>
+  );
+};
